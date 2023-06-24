0cc80d8fbc3cb05fbea62c4bc73a0e68232ea342
absal110 <aamohd@tekion.com>
2023-06-25 01:56:57 +0530

    step 2

--- fno.py
+++ fno.py
@@ -50,108 +50,108 @@ print(buy)
 print(sell)
 print(stt)
 
-# x='N'
-#
-# if buy:
-#     for each in buy:
-#         a = each[0].split()
-#         each[0] = a[0]
-# else:
-#     while 1:
-#         x = input("No purchase plese press Y")
-#         if x == 'Y':
-#             break
-#
-# for each in sell:
-#     a = each[0].split()
-#     each[0] = a[0]
-#
-# shares = []
-# for each in buy:
-#     if each[0] not in shares:
-#         shares.append(each[0])
-# for each in sell:
-#     if each[0] not in shares:
-#         shares.append(each[0])
-#
-#
-# dueToUs = ws[f'I{max}'].value
-# if dueToUs<0:
-#     dueToUs=dueToUs*-1
-# billDate = ws["A3"].value
-# billDate = billDate[12:]
-#
-# print(shares)
-#
-# # print(billDate)
-# print(dueToUs,stt)
-#
-# suffix = input("Enter Suffix if any or press 'ENTER'\n")
-#
-# time.sleep(2.5)
-#
-#
-# pyautogui.keyDown('alt')
-# time.sleep(0.25)
-# pyautogui.press('tab')
-# pyautogui.keyUp('alt')
-# time.sleep(0.65)
-# pyautogui.keyDown('alt')
-# pyautogui.press('a')
-# pyautogui.keyUp('alt')
-# pyautogui.press('f2')
-# pyautogui.typewrite(billDate)
-# pyautogui.press('enter')
-#
-# if x!='Y':
-#     pyautogui.typewrite("SHARE PURCHASE")
-#     pyautogui.press('enter')
-#     for each in buy:
-#         pyautogui.typewrite(each[0])
-#         if suffix!="":
-#             pyautogui.typewrite(suffix)
-#         pyautogui.press('enter')
-#         pyautogui.typewrite(str(each[1]))
-#         pyautogui.press('enter')
-#         pyautogui.press('enter')
-#         pyautogui.press('enter')
-#         pyautogui.typewrite(str(each[2]))
-#         pyautogui.press('enter')
-#     pyautogui.press('enter')
-#     pyautogui.typewrite("To")
-#     pyautogui.press('enter')
-#
-# if sell:
-#     pyautogui.typewrite("SHARE SALE")
-#     pyautogui.press('enter')
-#
-#     for each in sell:
-#         pyautogui.typewrite(each[0])
-#         if suffix != "":
-#             pyautogui.typewrite(suffix)
-#         pyautogui.press('enter')
-#         pyautogui.typewrite(str(each[1]))
-#         pyautogui.press('enter')
-#         pyautogui.press('enter')
-#         pyautogui.press('enter')
-#         pyautogui.typewrite(str(each[2]))
-#         pyautogui.press('enter')
-#     pyautogui.press('enter')
-#     pyautogui.press('enter')
-#     pyautogui.typewrite("BP")
-#     pyautogui.press('enter')
-#     pyautogui.typewrite(str(dueToUs))
-#     pyautogui.press('enter')
-#     pyautogui.press('enter')
-#     pyautogui.typewrite("STT")
-#     pyautogui.press('enter')
-#     pyautogui.typewrite(str(stt))
-#     pyautogui.press('enter')
-#     pyautogui.press('enter')
-#     pyautogui.typewrite("R")
-#     pyautogui.press('enter')
-#     pyautogui.press('enter')
-#     pyautogui.typewrite(fName)
-#     pyautogui.press('enter')
-#     time.sleep(0.75)
-#     pyautogui.press('enter')
+x='N'
+
+if buy:
+    for each in buy:
+        a = each[0].split()
+        each[0] = a[:1]
+else:
+    while 1:
+        x = input("No purchase plese press Y")
+        if x == 'Y':
+            break
+
+for each in sell:
+    a = each[0].split()
+    each[0] = a[:1]
+
+shares = []
+for each in buy:
+    if each[0] not in shares:
+        shares.append(each[0])
+for each in sell:
+    if each[0] not in shares:
+        shares.append(each[0])
+
+
+dueToUs = ws[f'H{max}'].value
+if dueToUs<0:
+    dueToUs=dueToUs*-1
+filekaname = ws["A3"].value
+billDate = filekaname[-10:]
+
+print(shares)
+
+# print(billDate)
+print(dueToUs,stt)
+
+suffix = input("Enter Suffix if any or press 'ENTER'\n")
+
+time.sleep(2.5)
+
+
+pyautogui.keyDown('alt')
+time.sleep(0.25)
+pyautogui.press('tab')
+pyautogui.keyUp('alt')
+time.sleep(0.65)
+pyautogui.keyDown('alt')
+pyautogui.press('a')
+pyautogui.keyUp('alt')
+pyautogui.press('f2')
+pyautogui.typewrite(billDate)
+pyautogui.press('enter')
+
+if x!='Y':
+    pyautogui.typewrite("SHARE PURCHASE")
+    pyautogui.press('enter')
+    for each in buy:
+        pyautogui.typewrite(each[0])
+        if suffix!="":
+            pyautogui.typewrite(suffix)
+        pyautogui.press('enter')
+        pyautogui.typewrite(str(each[1]))
+        pyautogui.press('enter')
+        pyautogui.press('enter')
+        pyautogui.press('enter')
+        pyautogui.typewrite(str(each[2]))
+        pyautogui.press('enter')
+    pyautogui.press('enter')
+    pyautogui.typewrite("To")
+    pyautogui.press('enter')
+
+if sell:
+    pyautogui.typewrite("SHARE SALE")
+    pyautogui.press('enter')
+
+    for each in sell:
+        pyautogui.typewrite(each[0])
+        if suffix != "":
+            pyautogui.typewrite(suffix)
+        pyautogui.press('enter')
+        pyautogui.typewrite(str(each[1]))
+        pyautogui.press('enter')
+        pyautogui.press('enter')
+        pyautogui.press('enter')
+        pyautogui.typewrite(str(each[2]))
+        pyautogui.press('enter')
+    pyautogui.press('enter')
+    pyautogui.press('enter')
+    pyautogui.typewrite("BP")
+    pyautogui.press('enter')
+    pyautogui.typewrite(str(dueToUs))
+    pyautogui.press('enter')
+    pyautogui.press('enter')
+    pyautogui.typewrite("STT")
+    pyautogui.press('enter')
+    pyautogui.typewrite(str(stt))
+    pyautogui.press('enter')
+    pyautogui.press('enter')
+    pyautogui.typewrite("R")
+    pyautogui.press('enter')
+    pyautogui.press('enter')
+    pyautogui.typewrite(fName)
+    pyautogui.press('enter')
+    time.sleep(0.75)
+    pyautogui.press('enter')
